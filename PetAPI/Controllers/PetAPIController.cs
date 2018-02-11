43d57d07812abeb2a0c37517aec2bad29a05a05f
cchaa22 <wtumtaweetikul@my.bcit.ca>
2018-02-10 23:27:18 -0800

    added post to pet api

--- PetAPI/Controllers/PetAPIController.cs
+++ PetAPI/Controllers/PetAPIController.cs
@@ -9,10 +9,11 @@ using PetAPI.Models;
 namespace PetAPI.Controllers
 {
     [Produces("application/json")]
-    [Route("api/PetAPI")]
+    //[Route("api/PetAPI")]
+    [Route("api/[controller]/[action]")]
     public class PetAPIController : Controller
     {
-        public List<LostVM> GetSampleData()
+        public List<LostVM> GetSampleLostData()
         {
             List<LostVM> lostAnimals = new List<LostVM>();
             lostAnimals.Add(new LostVM()
@@ -59,16 +60,109 @@ namespace PetAPI.Controllers
             return lostAnimals;
         }
 
-        [HttpGet]
-        public IEnumerable<LostVM> GetAll()
+        public List<FoundVM> GetSampleFoundData()
         {
-            return GetSampleData();
+            List<FoundVM> foundAnimals = new List<FoundVM>();
+            foundAnimals.Add(new FoundVM()
+            {
+                AnimalType = "Cat",
+                DateFound = new DateTime(2016, 10, 9),
+                Size = "Medium",
+                Color = "White",
+                Sex = "Male"
+            });
+            foundAnimals.Add(new FoundVM()
+            {
+                AnimalType = "Cat",
+                DateFound = new DateTime(2017, 5, 19),
+                Size = "Medium",
+                Color = "Black",
+                Sex = "Male"
+            });
+            foundAnimals.Add(new FoundVM()
+            {
+                AnimalType = "Cat",
+                DateFound = new DateTime(2017, 2, 24),
+                Size = "Large",
+                Color = "White",
+                Sex = "Female"
+            });
+            foundAnimals.Add(new FoundVM()
+            {
+                AnimalType = "Dog",
+                DateFound = new DateTime(2017, 8, 10),
+                Size = "Large",
+                Color = "White",
+                Sex = "Male"
+            });
+            foundAnimals.Add(new FoundVM()
+            {
+                AnimalType = "Dog",
+                DateFound = new DateTime(2017, 12, 12),
+                Size = "Medium",
+                Color = "Black",
+                Sex = "Female"
+            });
+            foundAnimals.Add(new FoundVM()
+            {
+                AnimalType = "Dog",
+                DateFound = new DateTime(2017, 7, 31),
+                Size = "Medium",
+                Color = "Dark Brown",
+                Sex = "Female"
+            });
+
+
+            return foundAnimals;
+        }
+
+        [HttpGet]
+        public IEnumerable<LostVM> GetAllLost()
+        {
+            return GetSampleLostData();
         }
 
         public IActionResult Index()
         {
 
-            return View(GetAll());
+            return View(GetAllLost());
+        }
+
+        
+        [HttpGet]
+        public IEnumerable<FoundVM> GetAllFound()
+        {
+            return GetSampleFoundData();
+        }
+
+        public IActionResult DisplayFound()
+        {
+
+            return View(GetAllFound());
+        }
+
+        [HttpPost]
+        public IActionResult CreateLostPet(LostVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                LostVM newDog = new LostVM
+                {
+                    AnimalType = model.AnimalType,
+                    DateLost = model.DateLost,
+                    Size = model.Size,
+                    Color = model.Color,
+                    Sex = model.Sex,
+                    Age = model.Age,
+                    Temperament = model.Temperament
+                };
+
+                GetSampleLostData().Add(newDog);
+                return Ok();
+
+            };
+            
+            return BadRequest();
         }
     }
 }
---
+++ PetAPI/Models/FoundVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetAPI.Models
+{
+    public class FoundVM
+    {
+        public string AnimalType { get; set; }
+        public DateTime DateFound { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public string Sex { get; set; }
+    }
+}
