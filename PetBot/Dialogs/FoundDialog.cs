f713d43b57d8f7f378e8ebeffb40f9c13c14bd7c
Lin Zhou <lzhou58@my.bcit.ca>
2018-02-10 18:45:42 -0800

    basic found dialog

---
+++ PetBot/Dialogs/FoundDialog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+
+namespace PetBot.Dialogs
+{
+    [Serializable]
+    public class FoundDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("We will need some info on the animal you just found.");
+
+        }
+    }
+}
